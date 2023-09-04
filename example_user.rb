48c87f765f1a55ef08127e46a30b346df80f81a4
nanopapa <88864315+nanopapa@users.noreply.github.com>
2023-09-04 22:39:47 +0000

    add example.user.rb 2023/09/05

---
+++ example_user.rb
@@ -0,0 +1,17 @@
+class User
+  attr_accessor :name, :email
+
+  def initialize(attributes = {})
+    @first_name  = attributes[:first_name]
+    @last_name  = attributes[:last_name]
+    @email = attributes[:email]
+  end
+
+  def formatted_email
+    "#{self.full_name} <#{@email}>"
+  end
+
+  def full_name
+    "#{@first_name}#{@last_name}"
+  end
+end
