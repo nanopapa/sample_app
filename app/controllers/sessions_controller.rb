1e9663a7e259878332b02952d34891abf625df0e
nanopapa <88864315+nanopapa@users.noreply.github.com>
2023-09-20 08:18:32 +0000

    from 8.2.4

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+      reset_session      # ログインの直前に必ずこれを書くこと
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      # エラーメッセージを作成する
+    render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  end
 end
