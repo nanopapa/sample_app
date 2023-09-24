624b59fcfe7cfc4084ead8d57bacd45dda70d769
nanopapa <88864315+nanopapa@users.noreply.github.com>
2023-09-24 21:06:56 +0000

    from9.3.2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,20 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user &.authenticate(params[:session][:password])
-      reset_session      
-      remember user
-      log_in user
-      redirect_to user
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_to @user
     else
-      flash.now[:danger] = 'Invalid email/password combination'
-    render 'new', status: :unprocessable_entity
+      flash.now[:danger] = 'Invalid email/password combination' 
+      render 'new'
     end
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url, status: :see_other
   end
 end
