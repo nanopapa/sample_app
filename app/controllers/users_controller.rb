f7895d93e4c9c5c5c0abaa600ed337c3c1b989a5
nanopapa <88864315+nanopapa@users.noreply.github.com>
2023-09-30 08:28:59 +0000

    from10.4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
   def show
     @user = User.find(params[:id])
     # debugger
     # デバッグの表示
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
   end
   
   def new
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,6 +6,11 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
+  end
+
   def setup
     @user = users(:michael)
     @other_user = users(:archer)
