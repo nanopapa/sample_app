e76dc59959ea2cfecdf7975015a437d8200a29db
nanopapa <88864315+nanopapa@users.noreply.github.com>
2023-10-01 23:06:15 +0000

    11.3前の演習

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -83,4 +83,12 @@ Rails.application.configure do
   config.action_cable.allowed_request_origins = ["https://#{pf_host}"]
     # クラウドIDE への接続を許可する
     config.hosts.clear
+
+    config.action_mailer.raise_delivery_errors = false
+
+    host = '<hex string>.app.github.dev'     # クラウドIDE
+     # クラウドIDEの場合は以下をお使いください
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+
+
 end
