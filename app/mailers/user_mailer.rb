b5b872e346a654d6e7bcd31740a95560e923bc31
nanopapa <88864315+nanopapa@users.noreply.github.com>
2023-10-04 21:13:18 +0000

    演習6から

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,9 +5,10 @@ def account_activation(user)
   mail to: user.email, subject: "Account activation"
 end
 
-  def password_reset
-    @greeting = "Hi"
+def password_reset(user)
+  @user = user
+  mail to: user.email, subject: "Password reset"
+end
 
-    mail to: "to@example.org"
-  end
+
 end
