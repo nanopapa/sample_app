e2042299d6d60c28899ec0e63b4b81d12db2c252
nanopapa <88864315+nanopapa@users.noreply.github.com>
2023-10-05 21:05:36 +0000

    from 12.3.2

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -29,7 +29,7 @@ class PasswordResetsController < ApplicationController
   def valid_user
     unless (@user && @user.activated? &&
             @user.authenticated?(:reset, params[:id]))
-      redirect_to root_url
+      # redirect_to root_url
     end
   end
 end
