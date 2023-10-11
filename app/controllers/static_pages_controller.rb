bebca146d6db4f9d2929d5b99f24010796529965
nanopapa <88864315+nanopapa@users.noreply.github.com>
2023-10-11 07:17:03 +0000

    site_layout_test.rb 30行　暫定コメントアウト　13.3．3前の演習から

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
+
   def home
+    @micropost = current_user.microposts.build if logged_in?
   end
 
   def help
@@ -11,4 +13,3 @@ class StaticPagesController < ApplicationController
   def contact
   end
 end
-
