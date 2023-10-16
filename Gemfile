4c5dc6cd5e6f931ab6cf5089bf01f4ebb2eedafb
nanopapa <88864315+nanopapa@users.noreply.github.com>
2023-10-16 20:50:19 +0000

    13.4.4

--- Gemfile
+++ Gemfile
@@ -42,8 +42,9 @@ group :test do
 end
 
 group :production do
-  gem "aws-sdk-s3", "1.114.0", require: false
+  gem "aws-sdk-s3", require: false
 end
 
 # Windows ではタイムゾーン情報用の tzinfo-data gem を含める必要があります
 #gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
+
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,8 +37,9 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
+
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :amazon
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
@@ -94,8 +95,7 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-    # アップロードされたファイルをAWSに保存する
-    config.active_storage.service = :amazon
+
 
   # Use a different logger for distributed setups.
   # require "syslog/logger"
