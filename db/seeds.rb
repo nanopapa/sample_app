d29583ae1a6404dce6497a05956086533b54ae30
nanopapa <88864315+nanopapa@users.noreply.github.com>
2023-10-21 07:02:42 +0000

    update sqlite

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,3 +28,11 @@ User.create!(name:  name,
   end
 
 end
+
+# ユーザーフォローのリレーションシップを作成する
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
