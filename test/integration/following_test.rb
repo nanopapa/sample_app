336dd8718084f6964a2c27a6da4b136aeb9d2ca6
nanopapa <88864315+nanopapa@users.noreply.github.com>
2023-10-22 02:27:36 +0000

    update sqlite

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -73,4 +73,12 @@ class UnfollowTest < Unfollow
       delete relationship_path(@relationship, format: :turbo_stream)
     end
   end
+
+  test "feed on Home page" do
+    get root_path
+    @user.feed.paginate(page: 1).each do |micropost|
+    assert_match CGI.escapeHTML(micropost.content), response.body
+    end
+  end
+
 end
